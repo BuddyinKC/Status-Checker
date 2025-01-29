3071177373f8130be139fcb3f076e8dba3f95900
Buddy Pennington <penningtonb@umkc.edu>
2025-01-29 10:39:18 -0600

    Updated script.

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import requests
  
+sites = [
+    {"name": "Library Website", "url": "https://library.umkc.edu"},
+    {"name": "Digital Collections (Islandora)", "url": "https://digital.library.umkc.edu"},
+    {"name": "Digital Exhibits (Omeka S)", "url": "https://exhibits.library.umkc.edu"},
+    {"name": "FOLIO", "url": "https://umsystem.folio.ebsco.com/"},
+]
+
+website_name = [
+    "",
+    "",
+    "",
+    "",
+]
+
 website_url = [
     "https://library.umkc.edu", 
     "https://digital.library.umkc.edu", 
-    "https://exhibits.library.umkc.edu", 
-
+    "https://exhibits.library.umkc.edu",
+    "https://umsystem.folio.ebsco.com/", 
 ]
  
 statuses = {
@@ -16,10 +30,21 @@ statuses = {
     503: "Service Unavailable"
 }
  
-for url in website_url:
+columns = ["Name", "URL", "Response"]
+
+table_head = f"<thead>\n<tr><th>{'</th><th>'.join(columns)}</th></tr>\n</thead>"
+table_body = "\n<tbody>\n"
+
+for site in website_url:
     try:
-        web_response = requests.get(url)
-        print(url, statuses[web_response.status_code])
+        web_response = requests.get(site)
+        print(site, statuses[web_response.status_code])
+        site_data = ["NAME", f"{site}", f"{statuses[web_response.status_code]}"]
+        table_body += f"<tr><td>{'</td><td>'.join(site_data)}</td></tr>\n"
  
     except:
-        print(url, statuses[web_response.status_code])
+        print("NAME", site, statuses[web_response.status_code])
+
+table_body += "</tbody>\n"
+
+print(f"<table>\n{table_head}{table_body}</table>")        
